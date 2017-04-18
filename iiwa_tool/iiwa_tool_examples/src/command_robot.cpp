b773c174d6b713ca5ddc05b8bbeef9fd07f3651c
Salvo Virga <salvo.virga@tum.de>
2017-04-18 18:25:02 +0200

    updated example for command with iiwa_ros

--- iiwa_tool/iiwa_tool_examples/src/command_robot.cpp
+++ iiwa_tool/iiwa_tool_examples/src/command_robot.cpp
@@ -1,25 +1,20 @@
-#include "ros/ros.h"
-#include "iiwa_msgs/JointPosition.h"
-#include "geometry_msgs/PoseStamped.h"
+#include <iiwa_ros.h>
+#include <cmath>
 
-iiwa_msgs::JointPosition current_joint_position, command_joint_position;
-geometry_msgs::PoseStamped current_cartesian_position, command_cartesian_position;
-std::string joint_position_topic, cartesian_position_topic, command_cartesian_position_topic, command_joint_position_topic;
-double ros_rate = 0.1;
-bool isRobotConnected = false, use_cartesian_command = true;
-
-void jointPositionCallback(const iiwa_msgs::JointPosition& jp)
-{
-	if (!isRobotConnected)
-		isRobotConnected = !isRobotConnected;
-	current_joint_position = jp;
-}
-
-void cartesianPositionCallback(const geometry_msgs::PoseStamped& ps)
-{
-	if (!isRobotConnected)
-		isRobotConnected = !isRobotConnected;
-	current_cartesian_position = ps;
+// getTimeToDestination() can also return negative values and the info from the cabinet take some milliseconds to update once the motion is started.
+// That means that if you call getTimeToDestination() right after you set a target pose, you might get the wrong info (e.g. a negative number).
+// This function tried to call getTimeToDestination() until something meaningful is obtained or until a maximum amount of time passed.
+void sleepForMotion(iiwa_ros::iiwaRos& iiwa, const double maxSleepTime) {
+  double ttd = iiwa.getTimeToDestinationService().getTimeToDestination();
+  ros::Time start_wait = ros::Time::now();
+  while (ttd < 0.0 && (ros::Time::now() - start_wait) < ros::Duration(maxSleepTime)) {
+    ros::Duration(0.5).sleep();
+    ttd = iiwa.getTimeToDestinationService().getTimeToDestination();
+  }
+  if (ttd > 0.0) {
+    ROS_INFO_STREAM("Sleeping for " << ttd << " seconds.");
+    ros::Duration(ttd).sleep();
+  } 
 }
 
 int main (int argc, char **argv) {
@@ -31,82 +26,49 @@ int main (int argc, char **argv) {
 	// ROS spinner.
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
+  
+  iiwa_ros::iiwaRos my_iiwa;
+  my_iiwa.init();
 	
 	// Dynamic parameters. Last arg is the default value. You can assign these from a launch file.
-	nh.param("joint_position_topic", joint_position_topic, std::string("/iiwa/state/JointPosition"));
-	nh.param("cartesian_position_topic", cartesian_position_topic, std::string("/iiwa/state/CartesianPose"));
-	nh.param("command_cartesian_position_topic", command_cartesian_position_topic, std::string("/iiwa/command/CartesianPose"));
-	nh.param("command_joint_position_topic", command_joint_position_topic, std::string("/iiwa/command/JointPosition"));
+  bool use_cartesian_command;
 	nh.param("use_cartesian_command", use_cartesian_command, true);
 	
 	// Dynamic parameter to choose the rate at wich this node should run
+  double ros_rate = 0.1;
 	nh.param("ros_rate", ros_rate, 0.1); // 0.1 Hz = 10 seconds
 	ros::Rate* loop_rate_ = new ros::Rate(ros_rate);
-	
-	// Subscribers and publishers
-	ros::Subscriber sub_joint_position = nh.subscribe(joint_position_topic, 1, jointPositionCallback);
-	ros::Subscriber sub_cartesian_position = nh.subscribe(cartesian_position_topic, 1, cartesianPositionCallback);
-	ros::Publisher pub_cartesian_command = nh.advertise<geometry_msgs::PoseStamped>(command_cartesian_position_topic, 1);
-	ros::Publisher pub_joint_command = nh.advertise<iiwa_msgs::JointPosition>(command_joint_position_topic, 1);
-	
+  
+  geometry_msgs::PoseStamped command_cartesian_position;
+  iiwa_msgs::JointPosition command_joint_position;
+  bool new_pose = false, motion_done = false;
+  
 	int direction = 1;
 	
 	while (ros::ok()) {
-		if (isRobotConnected) {
-			
-			// Printing out the current joint position
-			ROS_INFO("Current Joint Position is : [ %3.3f, %3.3f, %3.3f, %3.3f, %3.3f, %3.3f, %3.3f]", 
-					 current_joint_position.position.a1, 
-			current_joint_position.position.a2, 
-			current_joint_position.position.a3, 
-			current_joint_position.position.a4, 
-			current_joint_position.position.a5, 
-			current_joint_position.position.a6, 
-			current_joint_position.position.a7);
-			
-			// Printing out the current cartesian position
-			ROS_INFO("Current Cartesian Position is : [ %3.3f, %3.3f, %3.3f, %3.3f, %3.3f, %3.3f, %3.3f]", 
-					 current_cartesian_position.pose.position.x, 
-			current_cartesian_position.pose.position.y, 
-			current_cartesian_position.pose.position.z, 
-			current_cartesian_position.pose.orientation.x, 
-			current_cartesian_position.pose.orientation.y, 
-			current_cartesian_position.pose.orientation.z, 
-			current_cartesian_position.pose.orientation.w);
+    if (my_iiwa.getRobotIsConnected()) {
 			
 			if (use_cartesian_command) {
-				
-				// Here we set the new commanded cartesian position, we just move the tool TCP 10 centemeters down and back up, every 20 seconds.
-				command_cartesian_position = current_cartesian_position;
+        while (!my_iiwa.getCartesianPose(command_cartesian_position)) {}
+				// Here we set the new commanded cartesian position, we just move the tool TCP 10 centemeters down and back up, every 10 seconds.
 				command_cartesian_position.pose.position.z -= direction * 0.10;
-				
-				pub_cartesian_command.publish(command_cartesian_position); // Command position is published and executed by the robot (if the robot can achieve that position)
-				
+				my_iiwa.setCartesianPose(command_cartesian_position);				
 			}
 			else {
-				command_joint_position = current_joint_position;
-				command_joint_position.position.a4 -= direction * 0.0872665; // Adding/Subtracting 5 degrees (in radians) to the 4th joint
-				
-				pub_joint_command.publish(command_joint_position); // Command position is published and executed by the robot (if the joint limit is not exceeded)
-				
+        while (!my_iiwa.getJointPosition(command_joint_position)) {}
+        command_joint_position.position.a4 -= direction * 5 * M_PI / 180; // 0.0872665; // Adding/Subtracting 5 degrees (in radians) to the 4th joint
+				my_iiwa.setJointPosition(command_joint_position);
 			}
 			
-			direction *= -1; // In the next iteration the motion will be on the opposite direction
+			sleepForMotion(my_iiwa, 2.0);
 			
+			direction *= -1; // In the next iteration the motion will be on the opposite direction
 			
 			loop_rate_->sleep(); // Sleep for some millisecond. The while loop will run every 10 seconds in this example.
 		}
 		else {
-			ROS_ERROR("Robot is not connected...");
+			ROS_WARN_STREAM("Robot is not connected...");
 			ros::Duration(5.0).sleep(); // 5 seconds
 		}
-	}
-	
-	std::cerr<<"Stopping spinner..."<<std::endl;
-	spinner.stop();
-	
-	std::cerr<<"Bye!"<<std::endl;
-	
-	return 0;
-	
+	}	
 }; 
