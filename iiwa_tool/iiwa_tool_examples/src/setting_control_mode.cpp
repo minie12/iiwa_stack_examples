21c230d8e19a05fcab8b24637a9c8e015131d197
DaeunSong <daeun7250@gmail.com>
2020-07-25 14:26:09 +0900

    update examples to reflect changes in iiwa_ros

--- iiwa_tool/iiwa_tool_examples/src/setting_control_mode.cpp
+++ iiwa_tool/iiwa_tool_examples/src/setting_control_mode.cpp
@@ -1,46 +1,50 @@
-#include <iiwa_ros.h>
-#include <iiwa_ros/conversions.h>
+#include <iiwa_ros/state/cartesian_pose.hpp>
+#include <iiwa_ros/service/control_mode.hpp>
+#include <iiwa_ros/conversions.hpp>
 
 int main (int argc, char **argv) {
-  
+
   // Initialize ROS
   ros::init(argc, argv, "ControlMode");
   ros::NodeHandle nh("~");
-  
+
   // ROS spinner.
   ros::AsyncSpinner spinner(1);
   spinner.start();
-  
-  iiwa_ros::iiwaRos my_iiwa;
-  my_iiwa.init();
-   
+
+  iiwa_ros::state::CartesianPose iiwa_pose_state;
+  iiwa_ros::service::ControlModeService iiwa_control_mode;
+
+  iiwa_pose_state.init("iiwa");
+  iiwa_control_mode.init("iiwa");
+
   // Dynamic parameter to choose the rate at wich this node should run
   double ros_rate = 0.1;
   nh.param("ros_rate", ros_rate, 0.1); // 0.1 Hz = 10 seconds
-  ros::Rate* loop_rate_ = new ros::Rate(ros_rate);  
-  
+  ros::Rate* loop_rate_ = new ros::Rate(ros_rate);
+
   bool job_done = true;
-  
+
   while (job_done && ros::ok()) {
-    if (my_iiwa.getRobotIsConnected()) {
-      
+    if (iiwa_pose_state.isConnected()) {
+
       ROS_INFO_STREAM("The robot will be now set in Cartesian Impedance Mode");
       // Low stiffness only along Z.
-      my_iiwa.getSmartServoService().setCartesianImpedanceMode(iiwa_ros::cartesianQuantityFromDouble(1500,1500,350,300,300,300), iiwa_ros::cartesianQuantityFromDouble(0.7));
+      iiwa_control_mode.setCartesianImpedanceMode(iiwa_ros::conversions::CartesianQuantityFromFloat(1500,1500,350,300,300,300), iiwa_ros::conversions::CartesianQuantityFromFloat(0.7));
       ROS_INFO_STREAM("In 20 seconds the robot will go back to Position Control Mode");
       ros::Duration(20.0).sleep();
-      
-      my_iiwa.getSmartServoService().setPositionControlMode();
+
+      iiwa_control_mode.setPositionControlMode();
       ROS_INFO_STREAM("In 20 seconds the robot will be set in Joint Impedance Mode");
       ros::Duration(20.0).sleep();
-      
+
       // Low stiffness only for the 6th joint.
-      my_iiwa.getSmartServoService().setJointImpedanceMode(iiwa_ros::jointQuantityFromDouble(1500,1500,1500,1500,1500,0,1500), iiwa_ros::jointQuantityFromDouble(0.7));
+      iiwa_control_mode.setJointImpedanceMode(iiwa_ros::conversions::jointQuantityFromFloat(1500,1500,1500,1500,1500,0,1500), iiwa_ros::conversions::jointQuantityFromFloat(0.7));
       ROS_INFO_STREAM("In 20 seconds the robot will go back to Position Control Mode");
       ros::Duration(20.0).sleep();
-      
-      my_iiwa.getSmartServoService().setPositionControlMode();
-      
+
+      iiwa_control_mode.setPositionControlMode();
+
       ROS_INFO_STREAM("Job done! Shutting down...");
       job_done = false;
     }
@@ -49,5 +53,5 @@ int main (int argc, char **argv) {
       ros::Duration(5.0).sleep(); // 5 seconds
     }
   }
-  
-}; 
+};
+
